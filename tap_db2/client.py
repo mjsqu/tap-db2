4e83933a5e815f654e97b8b5aed081859c0bead9
Mark Johnston <mj2.squ@gmail.com>
2023-05-03 10:03:43 +0000

    Remove use of parameter for column name

--- tap_db2/client.py
+++ tap_db2/client.py
@@ -8,6 +8,7 @@ from __future__ import annotations
 from typing import Any, Iterable
 
 import sqlalchemy  # noqa: TCH002
+from sqlalchemy import bindparam
 from singer_sdk import SQLConnector, SQLStream
 
 
@@ -76,20 +77,57 @@ class db2Stream(SQLStream):
 
     connector_class = db2Connector
 
-    def get_records(self, partition: dict | None) -> Iterable[dict[str, Any]]:
+    def get_records(self, context: dict | None) -> t.Iterable[dict[str, t.Any]]:
         """Return a generator of record-type dictionary objects.
 
-        Developers may optionally add custom logic before calling the default
-        implementation inherited from the base class.
+        If the stream has a replication_key value defined, records will be sorted by the
+        incremental key. If the stream also has an available starting bookmark, the
+        records will be filtered for values greater than or equal to the bookmark value.
 
         Args:
-            partition: If provided, will read specifically from this data slice.
+            context: If partition context is provided, will read specifically from this
+                data slice.
 
         Yields:
             One dict per record.
+
+        Raises:
+            NotImplementedError: If partition is passed in context and the stream does
+                not support partitioning.
         """
-        # Optionally, add custom logic instead of calling the super().
-        # This is helpful if the source database provides batch-optimized record
-        # retrieval.
-        # If no overrides or optimizations are needed, you may delete this method.
-        yield from super().get_records(partition)
+        if context:
+            msg = f"Stream '{self.name}' does not support partitioning."
+            raise NotImplementedError(msg)
+
+        selected_column_names = self.get_selected_schema()["properties"].keys()
+        table = self.connector.get_table(
+            full_table_name=self.fully_qualified_name,
+            column_names=selected_column_names,
+        )
+        query = table.select()
+
+        if self.replication_key:
+            replication_key_col = table.columns[self.replication_key]
+            query = query.order_by(replication_key_col)
+
+            start_val = self.get_starting_replication_key_value(context)
+            if start_val:
+                # DB2 Parameters for column names not allowed
+                # https://www.ibm.com/docs/en/db2/11.5?topic=design-parameters-markers
+                query = query.where(
+                    sqlalchemy.text(f"{replication_key_col.name} >= :start_val").bindparams(
+                        start_val=start_val,
+                    ),
+                )
+            print(f"{query}")
+
+        if self.ABORT_AT_RECORD_COUNT is not None:
+            # Limit record count to one greater than the abort threshold. This ensures
+            # `MaxRecordsLimitException` exception is properly raised by caller
+            # `Stream._sync_records()` if more records are available than can be
+            # processed.
+            query = query.limit(self.ABORT_AT_RECORD_COUNT + 1)
+
+        with self.connector._connect() as conn:
+            for record in conn.execute(query):
+                yield dict(record._mapping)
