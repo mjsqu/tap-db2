2174ccc09b14d3739c08f1d4c242e2768d3c4ae2
Mark Johnston <mj2.squ@gmail.com>
2023-06-04 23:32:20 +1200

    Attempt db2sampl

--- tests/generate_data/staff_table_generate.sql
+++ tests/generate_data/staff_table_generate.sql
@@ -1,9 +1,8 @@
 -- Generate some sample data in the source database
 -- create a new table called TAP_TEST_500M 
-DROP TABLE TAP_TEST_500M;
 CREATE TABLE TAP_TEST_500M 
 (
-          		  "ID" SMALLINT NOT NULL , 
+        	  "ID" SMALLINT NOT NULL , 
 		  "NAME" VARCHAR(9 OCTETS) , 
 		  "DEPT" SMALLINT , 
 		  "JOB" CHAR(5 OCTETS) , 
@@ -14,7 +13,6 @@ CREATE TABLE TAP_TEST_500M
                                          (START WITH 100, INCREMENT BY 5)
 		  ) ;
 
-TRUNCATE TABLE  TAP_TEST_500M IMMEDIATE;
 
 -- This statement recursively generates rows from a cross-join to the STAFF table in the SAMPLE database
 -- adjust the WHERE clause to generate more rows - too high a value can fill the transaction log
@@ -26,15 +24,6 @@ SELECT 0 FROM SYSIBM.SYSDUMMY1
 SELECT I+1 FROM T WHERE I < 2000
 )
 SELECT Staff.*
-FROM T,STAFF;
-
-WITH T (I) AS 
-(
-SELECT 0 FROM SYSIBM.SYSDUMMY1
-    UNION ALL
-SELECT I+1 FROM T WHERE I <= 14284
-)
-SELECT count(*)
 FROM T,STAFF;
 
 SELECT count(*)
