b17f68ae9fdb63fc3646a5f8675e696c0d16f093
Mark Johnston <mj2.squ@gmail.com>
2023-06-05 02:49:30 +0000

    Setup testing

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,14 +1,12 @@
 """Tests standard tap features using the built-in SDK tests library."""
-
-import datetime
 import os
 
-from singer_sdk.testing import get_tap_test_class
+from singer_sdk.testing import SuiteConfig, get_tap_test_class
 
 from tap_db2.tap import Tapdb2
 from tap_db2.client import db2Connector
-import sqlalchemy
 
+from singer_sdk.testing import SuiteConfig, get_tap_test_class
 
 SAMPLE_CONFIG =  {'hostname': os.environ['TAP_DB2_HOSTNAME'],
                        'username': os.environ['TAP_DB2_USERNAME'],
@@ -17,16 +15,16 @@ SAMPLE_CONFIG =  {'hostname': os.environ['TAP_DB2_HOSTNAME'],
                        'database': os.environ['TAP_DB2_DATABASE'],
                        }
 
-
-conn = db2Connector(SAMPLE_CONFIG)
-print([stream['tap_stream_id'] for stream in conn.discover_catalog_entries()])
-
+# The sample database contains empty tables
+TEST_SUITE_CONFIG = SuiteConfig(
+    ignore_no_records=True
+)
 
 # Run standard built-in tap tests from the SDK:
-#TestTapdb2 = get_tap_test_class(
-#    tap_class=Tapdb2,
-#    config=SAMPLE_CONFIG,
-#)
-
+TestTapdb2 = get_tap_test_class(
+    tap_class=Tapdb2,
+    config=SAMPLE_CONFIG,
+    suite_config=TEST_SUITE_CONFIG,
+)
 
 # TODO: Create additional tests as appropriate for your tap.
